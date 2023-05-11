6ee03e822b1cd9e0e2b790b1975c6e5543d9b6b6
Hardik <singhhardik6014@gmail.com>
2023-05-11 17:57:06 +0530

    half done

--- myApp.js
+++ myApp.js
@@ -1,5 +1,29 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send("Hello Express");
+})
+
+const htmlPath = __dirname + '/views/index.html';
+
+app.get('/file', (req, res) => {
+    res.sendFile(htmlPath)
+})
+
+const publicPath = __dirname + '/public';
+
+app.use('/public', express.static(publicPath))
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({"message": "HELLO WORLD"})
+    } else {
+        res.json({"message": "Hello World!"})
+    }
+})
+
 
 
 
